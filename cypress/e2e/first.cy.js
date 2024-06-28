3f8c6d69c711e5ceb283de4eb8710b42c0598a08
Sreenu <sreenu.thiruveedi@gmail.com>
2024-06-29 00:01:47 +0530

    made modifications in firstcy

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,7 +1,9 @@
 describe("First one to perform practice with git integration", ()=>{
    it("TEst1",()=>{
     cy.visit('www.google.com')
-    cy.get(`input[placeholder='Search Google or type a URL']`).type("success")
+    cy.get(`DRc6kd.bdn4dc > button.M6CB1c.rr4y5c`).click()
+    cy.get(`[placeholder='Search Google or type a URL']`).type("success")
    }
 ) 
 })
+
