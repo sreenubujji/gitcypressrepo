faa9afe1016ebb0ef0ba84791bc738d408819e4a
Sreenu <sreenu.thiruveedi@gmail.com>
2024-06-29 19:49:04 +0530

    Modified

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,9 +1,13 @@
 describe("First one to perform practice with git integration", ()=>{
    it("TEst1",()=>{
     cy.visit('www.google.com')
-    cy.get(`DRc6kd.bdn4dc > button.M6CB1c.rr4y5c`).click()
-    cy.get(`[placeholder='Search Google or type a URL']`).type("success")
+    //cy.get(`DRc6kd.bdn4dc > button.M6CB1c.rr4y5c`).click()
+   //  cy.get(`[placeholder='Search Google or type a URL']`).type("success")
+   cy.get(`[aria-hidden="false"] > iframe button.M6CB1c.rr4y5c`).click()
    }
 ) 
 })
 
+
+
+// cy.get('[aria-hidden="false"] > iframe')
