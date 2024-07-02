42f63ba0c07338db78b31a93cc86b58e7a8c7447
Sreenu <sreenu.thiruveedi@gmail.com>
2024-07-02 21:24:25 +0530

    Lot of updates done

--- cypress/e2e/assignment.cy.js
+++ cypress/e2e/assignment.cy.js
@@ -1,20 +1,34 @@
+import { faker } from '@faker-js/faker';
+
 describe("part of Assignment-2",()=>{
     beforeEach(()=>{
         cy.visit('https://admin-demo.nopcommerce.com/login? ReturnUrl=%2Fadmin%2F')
+        // cy.fixture('loginData.json').as(`data`)
     })
 
-    it("To validate successfull login",()=>{
-        //cy.visit('https://admin-demo.nopcommerce.com/login? ReturnUrl=%2Fadmin%2F')
-        cy.get('input#Email').clear().type('admin@yourstore.com')
-        cy.get('input#Password').clear().type('admin')
+    it("To validate successfull login with fixture",()=>{            
+         cy.fixture('loginData.json').then((data) => {
+            cy.get('input#Email').clear().type(data.email)
+            cy.get('input#Password').clear().type(data.password)            
+          })
         cy.get('.button-1.login-button').click()
-        cy.get('.content-header > h1').contains(" Dashboard ")
+        cy.get('.content-header > h1').
+        then((element)=>{
+            const text=element.text().replace("\n","").trim()
+            cy.log(text)
+            // cy.wrap(element).should('have.text',text)
+            expect(text).to.equal("Dashboard")
+
+        })
+            // (('have.text',"Dashboard".))
     })
 
-    it("To validate the message showing incase of unsuccessfull login", ()=>{
+    it.only("To validate the message showing incase of unsuccessfull login", ()=>{
         //cy.visit('https://admin-demo.nopcommerce.com/login? ReturnUrl=%2Fadmin%2F')
-        cy.get('input#Email').clear().type('admin@yourstore.com')
-        cy.get('input#Password').clear().type('admin12')
+        let randomEmail = faker.internet.email();
+        let randomPassword = faker.internet.password();
+        cy.get('input#Email').clear().type(randomEmail)
+        cy.get('input#Password').clear().type(randomPassword)
         cy.get('.button-1.login-button').click()
         cy.get('div.message-error').should('have.text',"Login was unsuccessful. Please correct the errors and try again.The credentials provided are incorrect\n")
 
--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -5,7 +5,9 @@ describe("First one to perform practice with git integration", ()=>{
     cy.visit('www.google.com')
     //cy.get(`DRc6kd.bdn4dc > button.M6CB1c.rr4y5c`).click()
    //  cy.get(`[placeholder='Search Google or type a URL']`).type("success")
-   cy.frameLoaded(`button.M6CB1c.rr4y5c`).click()
+   cy.frameLoaded(`ifarme[name='callout']`)
+   //cy.iframe(`ifarme[name='callout']`).contains('Stay signed out').click()
+   cy.iframe(`ifarme[name='callout']`).find(`button[aria-label="Stay signed out"]`).should('be.visible').click()
    }
 ) 
 })
